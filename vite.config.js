4b3f2d86eb73f8ef3f206881534ae636f07527c0
waheednazeer <waheed.nazeer@hotmail.com>
2023-06-03 12:21:26 +0500

    .

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
+        main: resolve(__dirname, "src/index.html"),
         wb: resolve(__dirname, "src/json/wb.json"),
         country: resolve(__dirname, "src/json/country.json"),
 
